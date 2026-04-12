c091eb59e1c05d1f8c2a1c476b2c95a4c6a6dd20
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-12 17:10:01 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,4 @@
--- loader.lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sadboy-dev/Pidi/refs/heads/main/main.lua"))()
+local getRoleCode = game:HttpGet("https://raw.githubusercontent.com/sadboy-dev/Pidi/refs/heads/main/modules/getRole.lua")
+local getRoleModule = loadstring(getRoleCode)()
+local mainCode = game:HttpGet("https://raw.githubusercontent.com/sadboy-dev/Pidi/refs/heads/main/main.lua")
+loadstring(mainCode)(getRoleModule)
