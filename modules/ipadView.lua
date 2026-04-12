cd3d60861fca610cd60f7d463c9a30929a7b6a7c
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-12 17:49:53 +0800

    Update ipadView.lua

--- modules/ipadView.lua
+++ modules/ipadView.lua
@@ -1,19 +1,28 @@
 -- modules/ipadView.lua
 
 local Camera = workspace.CurrentCamera
-local defaultFOV = Camera.FieldOfView
+local defaultFOV = 70 -- Nilai standar kalau gagal baca
 
-local function on()
-    Camera.FieldOfView = 100
-    print("📱 iPad View: ON")
+-- Coba ambil FOV asli kalau bisa
+pcall(function()
+    defaultFOV = Camera.FieldOfView
+end)
+
+local function On()
+    pcall(function()
+        Camera.FieldOfView = 100
+        print("📱 iPad View: ON")
+    end)
 end
 
-local function off()
-    Camera.FieldOfView = defaultFOV
-    print("📱 iPad View: OFF")
+local function Off()
+    pcall(function()
+        Camera.FieldOfView = defaultFOV
+        print("📱 iPad View: OFF")
+    end)
 end
 
 return {
-    On = on,
-    Off = off
+    On = On,
+    Off = Off
 }
