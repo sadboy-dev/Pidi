181f5e96c748b056da72af740df66ffb392cc939
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-12 09:06:26 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 -- loader.lua
-loadstring(game:HttpGet("https://yourdomain.com/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sadboy-dev/Pidi/refs/heads/main/main.lua"))()
