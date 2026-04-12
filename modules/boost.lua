879936e5d101c88196117378dac561cd6f45af86
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-12 09:10:18 +0800

    Add boost module to manage lighting and particles

---
+++ modules/boost.lua
@@ -0,0 +1,23 @@
+local module = {}
+
+function module.Start()
+    local Lighting = game:GetService("Lighting")
+
+    pcall(function()
+        settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+    end)
+
+    Lighting.GlobalShadows = false
+    Lighting.FogEnd = 100000
+
+    for _,v in pairs(workspace:GetDescendants()) do
+        if v:IsA("ParticleEmitter") then
+            v.Enabled = false
+        end
+        if v:IsA("BasePart") then
+            v.Material = Enum.Material.Plastic
+        end
+    end
+end
+
+return module
